57be40a64e34c187f0f23a52068a1de50045696e
fevziomurtekin <fevziomurtekin@gmail.com>
2019-02-16 15:22:47 +0300

    layouts are being created.

---
+++ library/src/main/java/com/fevziomurtekin/custom_mapview/View.kt
@@ -0,0 +1,12 @@
+package com.fevziomurtekin.custom_mapview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class View : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_layout)
+    }
+}
