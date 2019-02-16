14d384047f78a220982f2f8e1a02a2ad0937f724
fevziomurtekin <fevziomurtekin@gmail.com>
2019-02-16 23:27:44 +0300

    location permission

--- library/src/main/java/com/fevziomurtekin/custom_mapview/View.kt
+++ library/src/main/java/com/fevziomurtekin/custom_mapview/View.kt
@@ -1,33 +1,49 @@
 package com.fevziomurtekin.custom_mapview
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 
+const val LOCATION = 1001
+
 open class View : AppCompatActivity(), OnMapReadyCallback {
 
-
     /*Map view*/
-    private var mapview : MapView=null
+    private lateinit var mapView: MapView
 
     private lateinit var mMap: GoogleMap
 
     /*Default focused location. (Turkey location) */
-    private val focus : LatLng = LatLng(44.5742, 25.90902)
+    private val focus : LatLng = LatLng(39.92077,32.85411)
 
+    private var mheight: Float = 0.toFloat()
+
+    private var mweight:Float = 0.toFloat()
+
+    private var scale:Float = 0.toFloat()
+
+    private val metrics = DisplayMetrics()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_layout)
 
-        checkLocation()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocation()
+        }
+
+        setDisplaySize()
 
         initMapView()
-
 
 
         /*TODO
@@ -37,20 +53,30 @@ open class View : AppCompatActivity(), OnMapReadyCallback {
         * arama kısmında ve menu kısmındaki açılır liste animasyonu yapılacak.*/
     }
 
+    private fun setDisplaySize() {
+        windowManager.defaultDisplay.getMetrics(metrics)
+        mheight = metrics.heightPixels.toFloat()
+        mweight = metrics.widthPixels.toFloat()
+        scale = metrics.density
+    }
+
     private fun initMapView() {
-        mapview = findViewById<MapView>(R.id.mapview) as MapView
-        if (mapview != null) {
-            mapview.onCreate(null)
-            mapview.onResume()
-            mapview.getMapAsync(this)
+        mapView = findViewById(R.id.mapview) as MapView
+        mapView.onCreate(null)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkLocation(){
+        if(this.checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && this.checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            this.requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION)
         }
     }
 
-    private fun checkLocation(){
-
-    }
-
     override fun onMapReady(p0: GoogleMap?) {
-
+        mMap = p0!!
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(focus,6f))
     }
 }
